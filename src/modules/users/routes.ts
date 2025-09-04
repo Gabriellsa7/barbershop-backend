a3121d9ccd54f1617b9a79bf90ef60aba2bda2c1
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2025-09-04 11:28:23 -0300

    feat: configured the delete, find by id and find by email routes

--- src/modules/users/routes.ts
+++ src/modules/users/routes.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
 import { createUserController } from "./controllers/create-user.controller";
 import { listUsersController } from "./controllers/list-users.controller";
+import { deleteUserController } from "./controllers/delete-user.controller";
+import { findUserByEmailController } from "./controllers/find-user-email.controller";
+import { findUserByIdController } from "./controllers/find-user-id.controller";
 
 const router = Router();
 
 router.post("/users", createUserController);
 router.get("/users", listUsersController);
+router.get("/users/:email", findUserByEmailController);
+router.get("/users/id/:id", findUserByIdController);
+router.delete("/users/:id", deleteUserController);
 
 export default router;
