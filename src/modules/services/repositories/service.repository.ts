b901d1a479b6830481c76af8722dd4cfb3d0b073
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2025-09-08 09:37:31 -0300

    feat: created the service repository

---
+++ src/modules/services/repositories/service.repository.ts
@@ -0,0 +1,22 @@
+// src/modules/services/repositories/service.repository.ts
+import { prisma } from "../../../config/prisma";
+
+export const serviceRepository = {
+  create: (data: {
+    name: string;
+    description?: string;
+    price: number;
+    durationMinutes: number;
+    barbershopId: string;
+  }) => {
+    return prisma.service.create({ data });
+  },
+
+  listByBarbershop: (barbershopId: string) => {
+    return prisma.service.findMany({ where: { barbershopId } });
+  },
+
+  delete: (id: string) => {
+    return prisma.service.delete({ where: { id } });
+  },
+};
