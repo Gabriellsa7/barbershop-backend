c1dc0a48a524fbc61ebb5a6651cf0fd49fa4e4ba
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2025-09-10 10:42:04 -0300

    feat: created the path to get all barbershops

--- src/modules/barbershops/routes.ts
+++ src/modules/barbershops/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { createBarbershopController } from "./controllers/create-barbershop.controller";
+import { getAllBarbershopController } from "./controllers/get-all-barbershop.controller";
 
 const router = Router();
 
 router.post("/barbershop", createBarbershopController);
+router.get("/barbershop", getAllBarbershopController);
 
 export default router;
