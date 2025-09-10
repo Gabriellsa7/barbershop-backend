6b2e6b45fd1167e2c0db49f99641a86f567bfee6
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2025-09-10 11:36:04 -0300

    feat: removed a unused export

--- src/modules/barbershops/controllers/get-all-barbershop.controller.ts
+++ src/modules/barbershops/controllers/get-all-barbershop.controller.ts
@@ -1,10 +1,7 @@
-import { Request, Response } from "express";
+import { Response } from "express";
 import { getAllBarbershopService } from "../services/get-all-barbershop.service";
 
-export const getAllBarbershopController = async (
-  req: Request,
-  res: Response
-) => {
+export const getAllBarbershopController = async (res: Response) => {
   try {
     const barbershops = await getAllBarbershopService();
     return res.status(200).json(barbershops);
