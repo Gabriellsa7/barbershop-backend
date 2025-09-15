0bc0218b56025cab02c7fef1e2bc6053b943eb12
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2025-09-15 09:57:10 -0300

    feat: fixed the import name and added the new router to find barbershop by id

--- src/modules/barbershops/routes.ts
+++ src/modules/barbershops/routes.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import { createBarbershopController } from "./controllers/create-barbershop.controller";
 import { getAllBarbershopController } from "./controllers/get-all-barbershop.controller";
-import { findBarbershopByOwnerContorller } from "./controllers/find-barbershop-by-owner.controller";
+import { findBarbershopByOwnerController } from "./controllers/find-barbershop-by-owner.controller";
+import { findBarbershopByIdController } from "./controllers/find-barbershop-by-id.controller";
 
 const router = Router();
 
 router.post("/barbershop", createBarbershopController);
 router.get("/barbershop", getAllBarbershopController);
-router.get("/barbershop/owner/:id", findBarbershopByOwnerContorller);
+router.get("/barbershop/owner/:id", findBarbershopByOwnerController);
+router.get("/barbershop/:id", findBarbershopByIdController);
 
 export default router;
