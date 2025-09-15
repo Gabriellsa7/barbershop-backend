74feb09ecd87677052f2b2cd5ac79c69da291bb7
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2025-09-15 09:32:30 -0300

    Feat: Created the controller to get all barbershops

--- src/modules/barbershops/controllers/get-all-barbershop.controller.ts
+++ src/modules/barbershops/controllers/get-all-barbershop.controller.ts
@@ -1,7 +1,10 @@
-import { Response } from "express";
+import { Request, Response } from "express";
 import { getAllBarbershopService } from "../services/get-all-barbershop.service";
 
-export const getAllBarbershopController = async (res: Response) => {
+export const getAllBarbershopController = async (
+  req: Request,
+  res: Response
+) => {
   try {
     const barbershops = await getAllBarbershopService();
     return res.status(200).json(barbershops);
