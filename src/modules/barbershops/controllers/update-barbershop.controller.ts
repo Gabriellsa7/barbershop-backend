8c62542dbca335ba1d3587c9cfa5b3e57c39f34f
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2025-09-16 10:33:47 -0300

    feat: created the controller with the logic to update the barbershop infos.

---
+++ src/modules/barbershops/controllers/update-barbershop.controller.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express";
+import { updateBarbershopService } from "../services/update-barbershop.service";
+
+export const updateBarbershopController = async (
+  req: Request,
+  res: Response
+) => {
+  try {
+    const { id } = req.params;
+    const data = req.body; // dados para atualizar
+
+    const updatedBarbershop = await updateBarbershopService(id, data);
+
+    return res.status(200).json(updatedBarbershop);
+  } catch (err: any) {
+    if (err.message === "You are not allowed to update this barbershop") {
+      return res.status(403).json({ error: err.message });
+    }
+
+    return res.status(400).json({ error: err.message });
+  }
+};
