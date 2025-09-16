0d38e6fb8a353d409172a03e17e11d2c67aeef4d
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2025-09-16 10:33:01 -0300

    feat: added a verification to know if the barbershp id exists.

--- src/modules/barbershops/services/delete-barbershop.service.ts
+++ src/modules/barbershops/services/delete-barbershop.service.ts
@@ -1,5 +1,8 @@
 import { barbershopRepository } from "../repositories/barbershop.repository";
 
 export const deleteBarbershopService = async (id: string) => {
+  if (!id) {
+    throw new Error("ID is required");
+  }
   return barbershopRepository.delete(id);
 };
