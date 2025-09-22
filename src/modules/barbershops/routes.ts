6123aeedc4ee87f48bcd7b79bcd664a7f16625b7
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2025-09-22 09:39:29 -0300

    feat: added the find nearby controller

--- src/modules/barbershops/routes.ts
+++ src/modules/barbershops/routes.ts
@@ -5,6 +5,7 @@ import { findBarbershopByOwnerController } from "./controllers/find-barbershop-b
 import { findBarbershopByIdController } from "./controllers/find-barbershop-by-id.controller";
 import { deleteBarbershopController } from "./controllers/delete-barbershop.controller";
 import { updateBarbershopController } from "./controllers/update-barbershop.controller";
+import { findBarbershopNearbyController } from "./controllers/find-barbershop-nearby.controller";
 
 const router = Router();
 
@@ -14,5 +15,6 @@ router.get("/barbershop/owner/:id", findBarbershopByOwnerController);
 router.get("/barbershop/:id", findBarbershopByIdController);
 router.delete("/barbershop/:id", deleteBarbershopController);
 router.put("/barbershop/:id", updateBarbershopController);
+router.get("/nearby", findBarbershopNearbyController);
 
 export default router;
