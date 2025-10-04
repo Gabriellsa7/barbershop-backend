4e904d82e9d0cc82d8eee6653590952a7ceac8b8
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2025-10-04 11:50:36 -0300

    feat: added new router

--- src/modules/users/routes.ts
+++ src/modules/users/routes.ts
@@ -4,10 +4,12 @@ import { listUsersController } from "./controllers/list-users.controller";
 import { deleteUserController } from "./controllers/delete-user.controller";
 import { findUserByEmailController } from "./controllers/find-user-email.controller";
 import { findUserByIdController } from "./controllers/find-user-id.controller";
+import { loginController } from "./controllers/login-user.controller";
 
 const router = Router();
 
 router.post("/users", createUserController);
+router.post("/login", loginController.login);
 router.get("/users", listUsersController);
 router.get("/users/:email", findUserByEmailController);
 router.get("/users/id/:id", findUserByIdController);
