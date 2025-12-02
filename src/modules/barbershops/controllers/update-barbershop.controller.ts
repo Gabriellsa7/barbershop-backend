9cebf314e328f315f76913c0ffdf3b0949d43208
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2025-12-02 16:18:27 -0300

    refactor: changed somethings

--- src/modules/barbershops/controllers/update-barbershop.controller.ts
+++ src/modules/barbershops/controllers/update-barbershop.controller.ts
@@ -1,22 +1,32 @@
-import { Request, Response } from "express";
-import { updateBarbershopService } from "../services/update-barbershop.service";
+import { Request, Response } from 'express';
+import { updateBarbershopService } from '../services/update-barbershop.service';
 
 export const updateBarbershopController = async (
   req: Request,
-  res: Response
+  res: Response,
 ) => {
   try {
     const { id } = req.params;
-    const data = req.body; // dados para atualizar
+    const data = req.body;
 
-    const updatedBarbershop = await updateBarbershopService(id, data);
+    const updatedBarbershop =
+      await updateBarbershopService(id, data);
 
-    return res.status(200).json(updatedBarbershop);
+    return res
+      .status(200)
+      .json(updatedBarbershop);
   } catch (err: any) {
-    if (err.message === "You are not allowed to update this barbershop") {
-      return res.status(403).json({ error: err.message });
+    if (
+      err.message ===
+      'You are not allowed to update this barbershop'
+    ) {
+      return res
+        .status(403)
+        .json({ error: err.message });
     }
 
-    return res.status(400).json({ error: err.message });
+    return res
+      .status(400)
+      .json({ error: err.message });
   }
 };
