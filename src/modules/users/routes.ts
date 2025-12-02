93a12807481f59cadfd70858e145c83b0cfae360
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2025-12-02 16:18:56 -0300

    feat: added update user router

--- src/modules/users/routes.ts
+++ src/modules/users/routes.ts
@@ -1,18 +1,26 @@
-import { Router } from "express";
-import { createUserController } from "./controllers/create-user.controller";
-import { listUsersController } from "./controllers/list-users.controller";
-import { deleteUserController } from "./controllers/delete-user.controller";
-import { findUserByEmailController } from "./controllers/find-user-email.controller";
-import { findUserByIdController } from "./controllers/find-user-id.controller";
-import { loginController } from "./controllers/login-user.controller";
+import { Router } from 'express';
+import { createUserController } from './controllers/create-user.controller';
+import { listUsersController } from './controllers/list-users.controller';
+import { deleteUserController } from './controllers/delete-user.controller';
+import { findUserByEmailController } from './controllers/find-user-email.controller';
+import { findUserByIdController } from './controllers/find-user-id.controller';
+import { loginController } from './controllers/login-user.controller';
+import { updateUserController } from './controllers/update-user.controller';
 
 const router = Router();
 
-router.post("/users", createUserController);
-router.post("/login", loginController.login);
-router.get("/users", listUsersController);
-router.get("/users/:email", findUserByEmailController);
-router.get("/users/id/:id", findUserByIdController);
-router.delete("/users/:id", deleteUserController);
+router.post('/users', createUserController);
+router.post('/login', loginController.login);
+router.get('/users', listUsersController);
+router.get(
+  '/users/:email',
+  findUserByEmailController,
+);
+router.get('/users/:id', findUserByIdController);
+router.delete(
+  '/users/id/:id',
+  deleteUserController,
+);
+router.put('/users/:id', updateUserController);
 
 export default router;
