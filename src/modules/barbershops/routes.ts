1ccf6176b3e0e4678c8e4591acb0eb4ec05700fb
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2025-12-02 16:18:44 -0300

    feat: created the barbershop routes

--- src/modules/barbershops/routes.ts
+++ src/modules/barbershops/routes.ts
@@ -1,20 +1,41 @@
-import { Router } from "express";
-import { createBarbershopController } from "./controllers/create-barbershop.controller";
-import { getAllBarbershopController } from "./controllers/get-all-barbershop.controller";
-import { findBarbershopByOwnerController } from "./controllers/find-barbershop-by-owner.controller";
-import { findBarbershopByIdController } from "./controllers/find-barbershop-by-id.controller";
-import { deleteBarbershopController } from "./controllers/delete-barbershop.controller";
-import { updateBarbershopController } from "./controllers/update-barbershop.controller";
-import { findBarbershopNearbyController } from "./controllers/find-barbershop-nearby.controller";
+import { Router } from 'express';
+import { createBarbershopController } from './controllers/create-barbershop.controller';
+import { getAllBarbershopController } from './controllers/get-all-barbershop.controller';
+import { findBarbershopByOwnerController } from './controllers/find-barbershop-by-owner.controller';
+import { findBarbershopByIdController } from './controllers/find-barbershop-by-id.controller';
+import { deleteBarbershopController } from './controllers/delete-barbershop.controller';
+import { updateBarbershopController } from './controllers/update-barbershop.controller';
+import { findBarbershopNearbyController } from './controllers/find-barbershop-nearby.controller';
 
 const router = Router();
 
-router.post("/barbershop", createBarbershopController);
-router.get("/barbershop", getAllBarbershopController);
-router.get("/barbershop/owner/:id", findBarbershopByOwnerController);
-router.get("/barbershop/:id", findBarbershopByIdController);
-router.delete("/barbershop/:id", deleteBarbershopController);
-router.put("/barbershop/:id", updateBarbershopController);
-router.get("/nearby", findBarbershopNearbyController);
+router.post(
+  '/barbershop',
+  createBarbershopController,
+);
+router.get(
+  '/barbershop',
+  getAllBarbershopController,
+);
+router.get(
+  '/barbershop/owner/:id',
+  findBarbershopByOwnerController,
+);
+router.get(
+  '/barbershop/:id',
+  findBarbershopByIdController,
+);
+router.delete(
+  '/barbershop/:id',
+  deleteBarbershopController,
+);
+router.put(
+  '/barbershop/:id',
+  updateBarbershopController,
+);
+router.get(
+  '/nearby',
+  findBarbershopNearbyController,
+);
 
 export default router;
