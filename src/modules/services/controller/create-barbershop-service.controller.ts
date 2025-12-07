91ace719c874eeee882c993ade6fd19d3a396ec1
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2025-12-07 13:20:46 -0300

    feat: created the method create the barbershop service and add image_url to service schema

---
+++ src/modules/services/controller/create-barbershop-service.controller.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from 'express';
+import { createBarbershopServiceService } from '../services/create-barbershop-service.service';
+
+export const createBarbershopServiceController =
+  async (req: Request, res: Response) => {
+    try {
+      const data = req.body;
+
+      if (!data) {
+        return res.status(400).json({
+          error:
+            'Name, price, durationMinutes, and barbershopId are required',
+        });
+      }
+
+      const service =
+        await createBarbershopServiceService(
+          data,
+        );
+
+      return res.status(201).json(service);
+    } catch (err: any) {
+      return res
+        .status(400)
+        .json({ error: err.message });
+    }
+  };
--- src/routes/router.ts
+++ src/routes/router.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import userRoutes from '../modules/users/routes';
 import barbershopRoutes from '../modules/barbershops/routes';
 import uploadRoutes from '../modules/uploads/routes';
+import barbershopServiceRoutes from '../modules/services/routes';
 
 const router = Router();
 
 router.use('/api', userRoutes);
 router.use('/api', barbershopRoutes);
 router.use('/api', uploadRoutes);
+router.use('/api', barbershopServiceRoutes);
 
 export default router;
