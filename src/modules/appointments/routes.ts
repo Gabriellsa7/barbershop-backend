65ed78d17d0784d26f543be183f9975853e8cddb
Gabriellsa7 <gabrielsantanasantos798@gmail.com>
2026-01-03 09:13:24 -0300

    feat: define the appointment router

---
+++ src/modules/appointments/routes.ts
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+import { createAppointmentController } from './controllers/create-appointment.controller';
+import { listAvailabilityController } from './controllers/availability.controller';
+
+const router = Router();
+
+router.post(
+  '/appointment',
+  createAppointmentController,
+);
+
+//Insomina Test Exemple
+//GET /appointments/availability?barbershopId=1&date=2026-01-10
+//GET /appointments/availability?barbershopId=aea64e59-fc8c-497d-9d91-0689fe2eb733&date=2026-01-02
+
+router.get(
+  '/appointments/availability',
+  listAvailabilityController,
+);
+
+export default router;
--- src/routes/router.ts
+++ src/routes/router.ts
@@ -3,6 +3,7 @@ import userRoutes from '../modules/users/routes';
 import barbershopRoutes from '../modules/barbershops/routes';
 import uploadRoutes from '../modules/uploads/routes';
 import barbershopServiceRoutes from '../modules/services/routes';
+import appointment from '../modules/appointments/routes';
 
 const router = Router();
 
@@ -10,5 +11,6 @@ router.use('/api', userRoutes);
 router.use('/api', barbershopRoutes);
 router.use('/api', uploadRoutes);
 router.use('/api', barbershopServiceRoutes);
+router.use('/api', appointment);
 
 export default router;
